388a335f028909cc9ad4677917c446dd3af950fb
sin <ogasawaraShinnosuke@users.noreply.github.com>
2016-12-19 22:10:48 +0900

    Update urls.py

--- django/tip/urls.py
+++ django/tip/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^selenium1', views.selenium_hatena, name='index'),
+    url(r'^selenium1', views.selenium_hatena_profile, name='index'),
 ]
